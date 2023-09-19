215cfa0f36e2acae20689e5a1d1176b98e8f80b0
unknown <you@example.com>
2023-09-19 12:25:52 -0500

    problem  one is solved

--- src/components/Problem-1.jsx
+++ src/components/Problem-1.jsx
@@ -1,62 +1,3 @@
-// import React, {useState} from 'react';
-
-// const Problem1 = () => {
-
-//     const [show, setShow] = useState('all');
-
-//     const handleClick = (val) =>{
-//         setShow(val);
-//     }
-
-//     return (
-
-//         <div className="container">
-//             <div className="row justify-content-center mt-5">
-//                 <h4 className='text-center text-uppercase mb-5'>Problem-1</h4>
-//                 <div className="col-6 ">
-//                     <form className="row gy-2 gx-3 align-items-center mb-4">
-//                         <div className="col-auto">
-//                             <input type="text" className="form-control" placeholder="Name"/>
-//                         </div>
-//                         <div className="col-auto">
-//                             <input type="text" className="form-control" placeholder="Status"/>
-//                         </div>
-//                         <div className="col-auto">
-//                             <button type="submit" className="btn btn-primary">Submit</button>
-//                         </div>
-//                     </form>
-//                 </div>
-//                 <div className="col-8">
-//                     <ul className="nav nav-pills mb-3" id="pills-tab" role="tablist">
-//                         <li className="nav-item">
-//                             <button  className={`nav-link ${show==='all' && 'active'}`} type="button" onClick={()=>handleClick('all')}>All</button>
-//                         </li>
-//                         <li className="nav-item">
-//                             <button className={`nav-link ${show==='active' && 'active'}`} type="button" onClick={()=>handleClick('active')}>Active</button>
-//                         </li>
-//                         <li className="nav-item">
-//                             <button  className={`nav-link ${show==='completed' && 'active'}`} type="button" onClick={()=>handleClick('completed')}>Completed</button>
-//                         </li>
-//                     </ul>
-//                     <div className="tab-content"></div>
-//                     <table className="table table-striped ">
-//                         <thead>
-//                         <tr>
-//                             <th scope="col">Name</th>
-//                             <th scope="col">Status</th>
-//                         </tr>
-//                         </thead>
-//                         <tbody>
-                        
-//                         </tbody>
-//                     </table>
-//                 </div>
-//             </div>
-//         </div>
-//     );
-// };
-
-// export default Problem1;
 import React, { useState } from 'react';
 
 const Problem1 = () => {
@@ -70,17 +11,38 @@ const Problem1 = () => {
 
   const handleFormSubmit = (e) => {
     e.preventDefault();
-    // Update the tableData state with the entered data
+  
     setTableData([...tableData, formData]);
-    // Clear the form fields
+   
     setFormData({ name: '', status: '' });
   };
+ const sortedTableData = [...tableData].sort((a, b) => {
+    const statusOrder = {
+      Active: 1,
+      Completed: 2,
+    };
+  
+    const statusA = statusOrder[a.status] || 3;
+    const statusB = statusOrder[b.status] || 3;
+  
+    if (statusA === statusB) {
+      return a.name.localeCompare(b.name); 
+    }
+  
+    return statusA - statusB;
+  });
+  const filteredTableData = sortedTableData.filter(item => {
+    if (show === 'all') return true;
+    if (show === 'active') return item.status === 'Active';
+    if (show === 'completed') return item.status === 'Completed';
+    return false;
+  });
 
   return (
     <div className="container">
       <div className="row justify-content-center mt-5">
         <h4 className="text-center text-uppercase mb-5">Problem-1</h4>
-        <div className="col-6 ">
+        <div className="col-6">
           <form
             className="row gy-2 gx-3 align-items-center mb-4"
             onSubmit={handleFormSubmit}
@@ -115,18 +77,17 @@ const Problem1 = () => {
           </form>
         </div>
         <div className="col-8">
-        <ul className="nav nav-pills mb-3" id="pills-tab" role="tablist">
-                       <li className="nav-item">
-                           <button  className={`nav-link ${show==='all' && 'active'}`} type="button" onClick={()=>handleClick('all')}>All</button>
-                        </li>
-                        <li className="nav-item">
-                            <button className={`nav-link ${show==='active' && 'active'}`} type="button" onClick={()=>handleClick('active')}>Active</button>
-                      </li>
-                        <li className="nav-item">
-                            <button  className={`nav-link ${show==='completed' && 'active'}`} type="button" onClick={()=>handleClick('completed')}>Completed</button>
-                       </li>
-                   </ul>
-<div className="tab-content"></div>
+          <ul className="nav nav-pills mb-3" id="pills-tab" role="tablist">
+            <li className="nav-item">
+              <button className={`nav-link ${show === 'all' && 'active'}`} type="button" onClick={() => handleClick('all')}>All</button>
+            </li>
+            <li className="nav-item">
+              <button className={`nav-link ${show === 'active' && 'active'}`} type="button" onClick={() => handleClick('active')}>Active</button>
+            </li>
+            <li className="nav-item">
+              <button className={`nav-link ${show === 'completed' && 'active'}`} type="button" onClick={() => handleClick('completed')}>Completed</button>
+            </li>
+          </ul>
           <table className="table table-striped">
             <thead>
               <tr>
@@ -135,12 +96,11 @@ const Problem1 = () => {
               </tr>
             </thead>
             <tbody>
-              {/* Map over the tableData state and render table rows */}
-              {tableData.map((item, index) => (
-                <tr key={index}>
-                  <td>{item.name}</td>
-                  <td>{item.status}</td>
-                </tr>
+            {filteredTableData.map((item, index) => (
+    <tr key={index}>
+      <td>{item.name}</td>
+      <td>{item.status}</td>
+    </tr>
               ))}
             </tbody>
           </table>
@@ -151,3 +111,5 @@ const Problem1 = () => {
 };
 
 export default Problem1;
+
+
